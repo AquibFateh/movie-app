43268ec6cb58d6858404d304755951bdcbce524b
AquibFateh <2012.khan.aquib@gmail.com>
2025-02-16 01:52:01 +0400

    Adds NavBar to the App

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,10 +1,10 @@
-function Favourite() {
+function Favourites() {
   return (
     <div className="favourites-empty">
-      <h2>No favourite Movies Yet</h2>
-      <p>Start adding movies to your favourites and it will appear here</p>
+      <h2>No Favourite Movies Yet</h2>
+      <p>Start adding movies to your favourites and it will appear here...</p>
     </div>
   );
 }
 
-export default Favourite;
+export default Favourites;
