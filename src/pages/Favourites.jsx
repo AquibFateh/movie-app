5c7db984aba57d90f76c7978f6a06349ee4f0f1c
AquibFateh <2012.khan.aquib@gmail.com>
2025-02-16 01:34:48 +0400

    Adds BrowserRouter to add routing in our app via reactreact-router-dom

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourite() {
+  return (
+    <div className="favourites-empty">
+      <h2>No favourite Movies Yet</h2>
+      <p>Start adding movies to your favourites and it will appear here</p>
+    </div>
+  );
+}
+
+export default Favourite;
